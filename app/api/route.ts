6c7f157976cbde143892f81d50e432d15778a57e
Patrick O'Leary <polea003@fiu.edu>
2023-12-21 15:11:17 -0500

    fix async

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,9 @@
 import sgMail from '@sendgrid/mail'
 
 export async function POST(req: Request) {
+  try {
     console.log('foo1')
     const { subscriberEmail } = await req.json()
-
     console.log(`foo2: ${subscriberEmail}`)
 
     sgMail.setApiKey(process.env.SENDGRID_API_KEY || '')
@@ -16,18 +16,14 @@ export async function POST(req: Request) {
       html: `<strong>${subscriberEmail} has signed up for updates</strong>`,
     }
     const msg2 = { ...msg, to: process.env.ADMIN_EMAIL_2}
-    
-    console.log('foo3')
 
-    sgMail
-      .send([msg, msg2])
-      .then(() => {
-        console.log('foo4')
-        console.log('Email sent')
-        return Response.json({ message: 'successfully subscribed' , ok: true })
-      })
-      .catch((error) => {
-        console.error(error)
-        return Response.json({ message: 'error occurred' , ok: false })
-      })
+    console.log('foo3')
+    sgMail.send([msg, msg2])
+    console.log('foo4')
+    console.log('Email sent')
+    return Response.json({ message: 'successfully subscribed' , ok: true })
+  } catch(e) {
+    console.log({e})
+    return Response.json({ message: 'error occurred' , ok: false })
   }
+}
